73001d7b9fe9caab8374e792b032075e9a3e6f33
Joe Wilcox <jawninety@gmail.com>
2026-02-02 20:01:00 +0000

    Change ordering slightly so call to super.__call__ is made sooner.

--- src/pymatk/controllers/pid_controller.py
+++ src/pymatk/controllers/pid_controller.py
@@ -7,15 +7,15 @@ from typing import Tuple
 
 class PIDController(PID):
     """
-    Docstring for PIDController
+    A PID controller that inherits from `simple_pid.pid.PID`.
     """
 
     def __init__(
         self,
-        Kp: float = 2,
+        Kp: float = 2.0,
         Ki: float = 0.05,
-        Kd: float = 0,
-        setpoint: float = 6,
+        Kd: float = 0.0,
+        setpoint: float = 6.0,
         sample_time: float = 0.01,
         starting_output: float = 0.2,
         output_limits: Tuple[float, float] = (0, 100),
@@ -23,6 +23,21 @@ class PIDController(PID):
         output_rate_limit: float | None = None,
         **kwargs,
     ):
+        """
+        Initialises a new PID controller inheriting from `simple_pid.pid.PID`.
+
+        :param deadband: Defines a band around the setpoint. When the input is
+            within the deadband, the output will not change from its last
+            value from outside the deadband.
+        :type deadband: float
+        :param output_rate_limit: Sets a nominal 'rate' to limit how fast the
+            output can change. This will override the calculated output of the
+            PID algorithm so that the output does not change by more than
+            `output_rate_limit` between updates.
+        :type output_rate_limit: float | None
+        :param kwargs: Keyword arguments to pass to the parent
+            `simple_pid.pid.PID`.
+        """
         super().__init__(
             Kp=Kp,
             Ki=Ki,
@@ -45,13 +60,13 @@ class PIDController(PID):
                   upper limit {output_limits[1]}."""
             )
 
-    def _is_in_deadband(self, input):
+    def _is_in_deadband(self, input) -> bool:
         if np.abs(input - self.setpoint) < self.deadband:
             return True
         else:
             return False
 
-    def _limit_rate(self, new_output: float, last_output: float):
+    def _limit_rate(self, new_output: float, last_output: float) -> float:
         if np.abs(last_output - new_output) > self.output_rate_limit:
             return last_output - self.output_rate_limit * np.sign(
                 last_output - new_output
@@ -60,17 +75,19 @@ class PIDController(PID):
             return new_output
 
     def __call__(self, input_: float, dt: float | None = None) -> float | None:
+        """
+        Modified PID update routine to include optional deadband and
+        rate-limiting functionality.
+        """
+        last_output = self._last_output
+        new_output = super().__call__(input_, dt)
         # Check if current measurement is within deadband of setpoint
         if self._is_in_deadband(input=input_) and (
-            self._last_output is not None
+            last_output is not None
         ):
-            # If in deadband, get last output
-            return self._last_output
+            # If in deadband, return last output
+            return last_output
         else:
-            # If not, get new output
-            last_output = self._last_output
-            new_output = super().__call__(input_, dt)
-
             # If output is rate limited, and new output is finite and last
             # output is not none
             if (
