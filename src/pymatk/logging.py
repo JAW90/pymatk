103473127b7792d95f34219caff04edca0791356
Joe Wilcox <jawninety@gmail.com>
2026-02-08 20:43:33 +0000

    Big overhaul of general software structure., extensive refactoring in attempt to simplify. Variables now belong to Instruments. Instruments can be added to an InstrumentRack. Experiments typically posess an InstrumentRack. Instead of DataFile, emply DataWriter. ConfigLoader is now ConfigParser, which returns configurations for the instruments etc, but its up to the experiment to create / manage the Instrument. Functionality is at it was previously.

---
+++ src/pymatk/logging.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+
+formatter = logging.Formatter(
+    "{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
